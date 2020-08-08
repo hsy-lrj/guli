6c5042e17843f22543e1a1209e60de733ebe721d
HSY <1224143192@qq.com>
2020-08-08 16:05:29 +0800

    day02_11

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -4,13 +4,17 @@ package com.atguigu.eduservice.controller;
 import com.atguigu.commonutils.result;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -20,7 +24,7 @@ import java.util.List;
  * @author atguigu
  * @since 2020-08-07
  */
-@Api(description="讲师管理")
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -29,17 +33,19 @@ public class EduTeacherController {
 
     /**
      * 讲师查询所有
+     *
      * @return
      */
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public result list(){
+    public result list() {
         List<EduTeacher> list = teacherService.list(null);
-        return result.ok().data("items",list);
+        return result.ok().data("items", list);
     }
 
     /**
      * 讲师逻辑删除
+     *
      * @param id
      * @return
      */
@@ -47,14 +53,34 @@ public class EduTeacherController {
     @DeleteMapping("{id}")
     public result removeById(
             @ApiParam(name = "id", value = "讲师ID", required = true)
-            @PathVariable String id){
+            @PathVariable String id) {
         boolean b = teacherService.removeById(id);
-        if (b){
+        if (b) {
             return result.ok();
-        }else {
+        } else {
             return result.error();
         }
     }
+    @ApiOperation(value = "讲师分页")
+    @GetMapping("pagrteacher/{current}/{limit}")
+    public result pageListTeacher(
+           @ApiParam(name = "current",value = "当前页数",required = true)
+           @PathVariable Integer current,
+           @ApiParam(name = "limit",value = "每页的数量",required = true)
+           @PathVariable Integer limit
+    ) {
+        //创建分页的条件
+        Page<EduTeacher> page = new Page<>(current, limit);
+        //调用方法
+        IPage<EduTeacher> eduTeacherIPage = teacherService.page(page, null);
+        long total = eduTeacherIPage.getTotal();
+        List<EduTeacher> records = eduTeacherIPage.getRecords();
+        Map map = new HashMap();
+        map.put("total",total);
+        map.put("records",records);
+        return result.ok().data(map);
+    }
+
 
 }
 
