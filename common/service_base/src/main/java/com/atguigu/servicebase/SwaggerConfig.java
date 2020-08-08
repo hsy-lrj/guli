86e6913fc3459fe63cba07c3ab6e23537ed2bc50
HSY <1224143192@qq.com>
2020-08-08 22:49:33 +0800

    day02_16

--- common/service_base/src/main/java/com/atguigu/servicebase/SwaggerConfig.java
+++ common/service_base/src/main/java/com/atguigu/servicebase/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.atguigu.servicebase.config;
+package com.atguigu.servicebase;
 
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,13 +3,16 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.commonutils.result;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -26,7 +29,7 @@ import java.util.Map;
  */
 @Api(description = "讲师管理")
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/edu_teacher")
 public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
@@ -44,41 +47,155 @@ public class EduTeacherController {
     }
 
     /**
-     * 讲师逻辑删除
+     * 根据ID逻辑删除讲师
      *
      * @param id
      * @return
      */
-    @ApiOperation(value = "根据ID删除讲师")
+    @ApiOperation(value = "根据ID逻辑删除讲师")
     @DeleteMapping("{id}")
     public result removeById(
             @ApiParam(name = "id", value = "讲师ID", required = true)
             @PathVariable String id) {
-        boolean b = teacherService.removeById(id);
-        if (b) {
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
             return result.ok();
         } else {
             return result.error();
         }
     }
+
+    /**
+     * 讲师分页
+     * @param page
+     * @param limit
+     * @return
+     */
     @ApiOperation(value = "讲师分页")
-    @GetMapping("pagrteacher/{current}/{limit}")
+    @GetMapping("pagrteacher/{page}/{limit}")
     public result pageListTeacher(
-           @ApiParam(name = "current",value = "当前页数",required = true)
-           @PathVariable Integer current,
-           @ApiParam(name = "limit",value = "每页的数量",required = true)
-           @PathVariable Integer limit
+            @ApiParam(name = "page", value = "当前页数", required = true)
+            @PathVariable Integer page,
+            @ApiParam(name = "limit", value = "每页的数量", required = true)
+            @PathVariable Integer limit
     ) {
         //创建分页的条件
-        Page<EduTeacher> page = new Page<>(current, limit);
+        Page<EduTeacher> p = new Page<>(page, limit);
         //调用方法
-        IPage<EduTeacher> eduTeacherIPage = teacherService.page(page, null);
+        IPage<EduTeacher> eduTeacherIPage = teacherService.page(p, null);
         long total = eduTeacherIPage.getTotal();
         List<EduTeacher> records = eduTeacherIPage.getRecords();
         Map map = new HashMap();
-        map.put("total",total);
-        map.put("records",records);
+        map.put("total", total);
+        map.put("records", records);
         return result.ok().data(map);
+    }
+
+    /**
+     * 多条件查询带分页
+     * @param page
+     * @param limit
+     * @param teacherQuery
+     * @return
+     */
+    @ApiOperation("多条件查询带分页")
+    @PostMapping("pageTeacherCondition/{page}/{limit}")
+    public result pageTeacherCondition(
+            @ApiParam(name = "page", value = "当前页数", required = true)
+            @PathVariable("page") Integer page,
+            @ApiParam(name = "limit", value = "总页数", required = true)
+            @PathVariable("limit") Integer limit,
+            @RequestBody(required = false) TeacherQuery teacherQuery
+    ) {
+        //创建分页对象
+        Page<EduTeacher> p = new Page<EduTeacher>(page, limit);
+        //构建条件
+        QueryWrapper<EduTeacher> queryWrapper = new QueryWrapper<EduTeacher>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断字段并添加
+        if(!StringUtils.isEmpty(name)){
+            queryWrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            queryWrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)){
+            queryWrapper.ge("gmt_create", begin);
+        }
+        if(!StringUtils.isEmpty(end)){
+            queryWrapper.le("gmt_create", end);
+        }
+        //调用方法
+        IPage<EduTeacher> teacherIPage = teacherService.page(p,queryWrapper);
+        long total = teacherIPage.getTotal();
+        List<EduTeacher> records = teacherIPage.getRecords();
+        Map map = new HashMap();
+        map.put("total", total);
+        map.put("records", records);
+        return result.ok().data(map);
+    }
+
+    /**
+     * 新增讲师
+     * @param teacher
+     * @return
+     */
+    @ApiOperation(value = "新增讲师")
+    @PostMapping
+    public result save(
+            @ApiParam(name = "teacher", value = "讲师对象", required = true)
+            @RequestBody EduTeacher teacher){
+
+        boolean flag = teacherService.save(teacher);
+        if (flag) {
+            return result.ok();
+        } else {
+            return result.error();
+        }
+
+
+    }
+
+    /**
+     * 根据ID查询讲师
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "根据ID查询讲师")
+    @GetMapping("findById/{id}")
+    public result getById(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id){
+
+        EduTeacher teacher = teacherService.getById(id);
+        return result.ok().data("item", teacher);
+
+    }
+
+    /**
+     * 根据ID修改讲师
+     * @param id
+     * @param teacher
+     * @return
+     */
+    @ApiOperation(value = "根据ID修改讲师")
+    @PutMapping("updateById/{id}")
+    public result updateById(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id,
+            @ApiParam(name = "teacher", value = "讲师对象", required = true)
+            @RequestBody EduTeacher teacher){
+
+        teacher.setId(id);
+        boolean flag = teacherService.updateById(teacher);
+        if (flag) {
+            return result.ok();
+        } else {
+            return result.error();
+        }
     }
 
 
